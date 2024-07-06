3610cf657a83ec5bd33fbb218e1868122ac7f848
Pedro Barreto <pbdeoliveira@gmail.com>
2024-07-06 12:06:40 -0300

    projects and video routes

--- src/server.ts
+++ src/server.ts
@@ -1,8 +1,9 @@
 import express from 'express';
 import getPosts from './controllers/blogPostsController';
+import getProjects from './controllers/projectsController';
+import getVideos from './controllers/videosController';
 import bodyParser from 'body-parser';
 import cors from 'cors';
-
 import dotenv from 'dotenv';
 
 dotenv.config();
@@ -12,7 +13,8 @@ app.use(bodyParser.json());
 app.use(cors());
 const port = 10000;
 
-
 app.post('/posts', getPosts);
+app.post('/projects', getProjects);
+app.post('/videos', getVideos);
+
 app.listen(port, () => console.log(`funcionando na porta: ${port}!`));
-  
